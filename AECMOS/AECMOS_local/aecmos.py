0cfcae00c08876628d18569313332d4c7446b409
Marju Purin <marjupurin@microsoft.com>
2022-09-26 12:17:44 +0300

    add new decmos .onnx models

--- AECMOS/AECMOS_local/aecmos.py
+++ AECMOS/AECMOS_local/aecmos.py
@@ -7,12 +7,19 @@ import argparse
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--talk_type', type=str, required=True,
-                        help="Specify the scenario: 'st' (far-end single talk), 'nst' (near-end single talk), or 'dt' (double talk).")
-    parser.add_argument('--model_path', type=str, help="Specify the path to the onnx model provided")
-    parser.add_argument('--lpb_path', type=str, required = True, help= "Specify the path to the lpb audio file")
-    parser.add_argument('--mic_path', type=str, required = True, help= "Specify the path to the mic audio file")
-    parser.add_argument('--enh_path', type=str, required = True, help= "Specify the path to the enh audio file")
+    parser.add_argument(
+        '--talk_type',
+        type=str,
+        default=None,
+        help="Specify the scenario: 'st' (far-end single talk), 'nst' (near-end single talk), or 'dt' (double talk).")
+    parser.add_argument('--model_path', type=str,
+                        help="Specify the path to the onnx model provided")
+    parser.add_argument('--lpb_path', type=str, required=True,
+                        help="Specify the path to the lpb audio file")
+    parser.add_argument('--mic_path', type=str, required=True,
+                        help="Specify the path to the mic audio file")
+    parser.add_argument('--enh_path', type=str, required=True,
+                        help="Specify the path to the enh audio file")
     return parser.parse_args()
 
 
@@ -24,15 +31,18 @@ class AECMOSEstimator():
         self.model_path = model_path
         self.max_len = 20
         self.sampling_rate = 16000
-        if 'Run1644323924_Stage-0' in self.model_path:
+        if 'Run_1663915512_Stage_0' in self.model_path:
             self.transform = self._mel_transform
-        elif 'Run_1657188842_Stage_0' in self.model_path:
+            self.need_scenario_marker = True
+        elif 'Run_1663829550_Stage_0' in self.model_path:
             self.transform = self._mel_transform
+            self.need_scenario_marker = False
         else:
             ValueError, "Not a supported model."
 
     def _mel_transform(self, sample, sr):
-        mel_spec = librosa.feature.melspectrogram(y=sample, sr=sr, n_fft=512 + 1, hop_length=256, n_mels=160)
+        mel_spec = librosa.feature.melspectrogram(
+            y=sample, sr=sr, n_fft=512 + 1, hop_length=256, n_mels=160)
         mel_spec = (librosa.power_to_db(mel_spec, ref=np.max) + 40) / 40
         return mel_spec.T
 
@@ -64,23 +74,25 @@ class AECMOSEstimator():
         enh_sig = self.transform(enh_sig, self.sampling_rate)
 
         # scenario marker
-        assert talk_type in ['nst', 'st', 'dt']
+        if self.need_scenario_marker:
+            assert talk_type in ['nst', 'st', 'dt']
 
-        if talk_type == 'nst':
-            ne_st = 1
-            fe_st = 0
-        elif talk_type == 'st':
-            ne_st = 0
-            fe_st = 1
-        else:
-            ne_st = 0
-            fe_st = 0
+            if talk_type == 'nst':
+                ne_st = 1
+                fe_st = 0
+            elif talk_type == 'st':
+                ne_st = 0
+                fe_st = 1
+            else:
+                ne_st = 0
+                fe_st = 0
 
-        mic_sig = np.concatenate(
-            (mic_sig, np.ones((20, mic_sig.shape[1])) * (1 - fe_st), np.zeros((20, mic_sig.shape[1]))), axis=0)
-        lpb_sig = np.concatenate(
-            (lpb_sig, np.ones((20, lpb_sig.shape[1])) * (1 - ne_st), np.zeros((20, lpb_sig.shape[1]))), axis=0)
-        enh_sig = np.concatenate((enh_sig, np.ones((20, enh_sig.shape[1])), np.zeros((20, enh_sig.shape[1]))), axis=0)
+            mic_sig = np.concatenate(
+                (mic_sig, np.ones((20, mic_sig.shape[1])) * (1 - fe_st), np.zeros((20, mic_sig.shape[1]))), axis=0)
+            lpb_sig = np.concatenate(
+                (lpb_sig, np.ones((20, lpb_sig.shape[1])) * (1 - ne_st), np.zeros((20, lpb_sig.shape[1]))), axis=0)
+            enh_sig = np.concatenate((enh_sig, np.ones(
+                (20, enh_sig.shape[1])), np.zeros((20, enh_sig.shape[1]))), axis=0)
 
         # stack
         feats = np.stack((lpb_sig, mic_sig, enh_sig)).astype(np.float32)
@@ -103,11 +115,12 @@ class AECMOSEstimator():
 
 if __name__ == "__main__":
     args = parse_args()
-    
-    model_path = "Run1644323924_Stage-0.onnx"
+
+    model_path = args.model_path 
     aecmos = AECMOSEstimator(model_path)
-    
+
     lpb_sig, mic_sig, enh_sig = aecmos.read_and_process_audio_files(
-            args.lpb_path, args.mic_path, args.enh_path)
+        args.lpb_path, args.mic_path, args.enh_path)
     scores = aecmos.run(args.talk_type, lpb_sig, mic_sig, enh_sig)
-    print(f'The AECMOS echo score is {scores[0]}, and (other) degradation score is {scores[1]}.')
+    print(
+        f'The AECMOS echo score is {scores[0]}, and (other) degradation score is {scores[1]}.')
